3bfd664d44ed4e75e9a63430474f92263b664240
saroj raut <sarojraut@Saroj.local>
2024-08-09 11:55:36 +0545

    json update logi

--- mrss.py
+++ mrss.py
@@ -467,6 +467,13 @@ def delete_and_create_new_layout(arr):
 
 delete_and_create_new_layout(replacement_array)
 
+file_path = 'output_array.json'
+if replacement_array:
+        # Save the array to a JSON file, replacing it if it already exists
+    with open(file_path, 'w') as json_file:
+        json.dump(teams_videos, json_file, indent=4)
+        print(f"Data saved to {file_path}")
+
 # download the mrss feed into a file
 # match the old file with new file , only check similar title video name
 # if video is changed , create a array , put layout id to delete and match name and video name . 
@@ -474,7 +481,7 @@ delete_and_create_new_layout(replacement_array)
 
 #downloading the mrss feed and saving it in the local storage
 # Define the file path
-file_path = 'output_array.json'
+
 
 # Save the array to a JSON file
 # Save the array to a JSON file only if the file doesn't exist
